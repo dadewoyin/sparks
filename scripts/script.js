c62715687164c7ae45982700ee101453da0c8693
Robert Battaglia <r083rt164@gmail.com>
2019-10-19 14:01:39 -0400

    select choice functionality

--- scripts/script.js
+++ scripts/script.js
@@ -7,35 +7,35 @@
 // Changelogs - https://fb.me/spark-changelog
 //==============================================================================
 
-
 const story = {
   '': {
     word: '',
+    after: [],
     children: {
-      'Jenny': {
+      Jenny: {
         after: ['went', 'to', 'the'],
         children: {
-          'beach': {
+          beach: {
             after: ['and', 'they', 'forgot', 'their'],
             children: {
-              'cat': {
+              cat: {
                 '.': '.'
               },
-              'cheetos': {
+              cheetos: {
                 '.': '.'
               },
-              'towels': {
+              towels: {
                 '.': '.'
               }
             }
           },
-          'mall': {
+          mall: {
             after: ['and', 'they', 'bought', 'new'],
             children: {
-              'slippers': {
+              slippers: {
                 '.': '.'
               },
-              'toothpaste': {
+              toothpaste: {
                 '.': '.'
               },
               'Facebook Portals': {
@@ -43,46 +43,46 @@ const story = {
               }
             }
           },
-          'castle': {
+          castle: {
             after: ['and', 'fell', 'into', 'the'],
             children: {
-              'well': {
+              well: {
                 '.': '.'
               },
-              'toilet': {
+              toilet: {
                 '.': '.'
               },
-              'moat': {
+              moat: {
                 '.': '.'
               }
             }
           }
         }
       },
-      'Jasmine': {
+      Jasmine: {
         after: ['went', 'to', 'the'],
         children: {
-          'beach': {
+          beach: {
             after: ['and', 'they', 'forgot', 'their'],
             children: {
-              'cat': {
+              cat: {
                 '.': '.'
               },
-              'cheetos': {
+              cheetos: {
                 '.': '.'
               },
-              'towels': {
+              towels: {
                 '.': '.'
               }
             }
           },
-          'mall': {
+          mall: {
             after: ['and', 'they', 'bought', 'new'],
             children: {
-              'slippers': {
+              slippers: {
                 '.': '.'
               },
-              'toothpaste': {
+              toothpaste: {
                 '.': '.'
               },
               'Facebook Portals': {
@@ -90,46 +90,46 @@ const story = {
               }
             }
           },
-          'castle': {
+          castle: {
             after: ['and', 'fell', 'into', 'the'],
             children: {
-              'well': {
+              well: {
                 '.': '.'
               },
-              'toilet': {
+              toilet: {
                 '.': '.'
               },
-              'moat': {
+              moat: {
                 '.': '.'
               }
             }
           }
         }
       },
-      'Antonio': {
+      Antonio: {
         after: ['went', 'to', 'the'],
         children: {
-          'beach': {
+          beach: {
             after: ['and', 'they', 'forgot', 'their'],
             children: {
-              'cat': {
+              cat: {
                 '.': '.'
               },
-              'cheetos': {
+              cheetos: {
                 '.': '.'
               },
-              'towels': {
+              towels: {
                 '.': '.'
               }
             }
           },
-          'mall': {
+          mall: {
             after: ['and', 'they', 'bought', 'new'],
             children: {
-              'slippers': {
+              slippers: {
                 '.': '.'
               },
-              'toothpaste': {
+              toothpaste: {
                 '.': '.'
               },
               'Facebook Portals': {
@@ -137,16 +137,16 @@ const story = {
               }
             }
           },
-          'castle': {
+          castle: {
             after: ['and', 'fell', 'into', 'the'],
             children: {
-              'well': {
+              well: {
                 '.': '.'
               },
-              'toilet': {
+              toilet: {
                 '.': '.'
               },
-              'moat': {
+              moat: {
                 '.': '.'
               }
             }
@@ -155,9 +155,7 @@ const story = {
       }
     }
   }
-}
-
-
+};
 
 // How to load in modules
 const Diagnostics = require('Diagnostics');
@@ -173,53 +171,65 @@ storyText.text = 'Tap to play game';
 const optionText = Scene.root.find('2dTextOption');
 optionText.text = '';
 
-let isPlaying = false
+let isPlaying = false;
 
 function changeOptionText(choices, i) {
-  optionText.text = choices[i]
+  optionText.text = choices[i];
 }
 
-TouchGestures.onTap().subscribeWithSnapshot({
-  // Get the value of mouth openness when the tap gesture is detected
-  'mouthOpennessValue': FaceTracking.face(0).mouth.openness,
-}, (gesture, snapshot) => {
+TouchGestures.onTap().subscribeWithSnapshot(
+  {
+    // Get the value of mouth openness when the tap gesture is detected
+    mouthOpennessValue: FaceTracking.face(0).mouth.openness
+  },
+  (gesture, snapshot) => {
+    isPlaying = !isPlaying;
+    Diagnostics.log(isPlaying);
 
-  isPlaying = !isPlaying
-  Diagnostics.log(isPlaying)
+    if (isPlaying) {
+      storyText.text = '';
+      // init story trie
+      let root = { ...story }[''];
+      let choices = Object.keys(root.children);
+      // let choosing = true
+      Diagnostics.log('choices: ');
+      Diagnostics.log(choices);
 
-  if (isPlaying) {
+      // loop through and show choices
+      let i = 0;
 
-    // init story trie
-    let root = {...story}
-    let choices = Object.keys(root[''].children)
-    // let choosing = true
-    Diagnostics.log('choices: ')
-    Diagnostics.log(choices)
+      const intervalTimer = Time.setInterval(() => {
+        i = (i + 1) % 3;
+        changeOptionText(choices, i);
+      }, 500);
 
-    // loop through and show choices
-    let i = 0
+      let chosen = false;
 
-    const intervalTimer = Time.setInterval(() => {
-      i = (i + 1) % 3
-      changeOptionText(choices, i)
-    }, 500);
+      // subscribe to mouth
+      FaceTracking.face(0)
+        .mouth.openness.monitor({ fireOnInitialValue: true })
+        .subscribe(event => {
+          let openness = event.newValue;
+          if (openness > 0.3 && isPlaying && !chosen) {
+            // select word
+            Time.clearInterval(intervalTimer);
+            const chosenWord = choices[i];
+            Diagnostics.log('you picked: ');
+            Diagnostics.log(chosenWord);
+            chosen = true;
+            root = root.children[chosenWord];
+            storyText.text = `${storyText.text.pinLastValue()} ${chosenWord} ${root.after.join(
+              ' '
+            )}`;
+          }
+        });
 
-    // subscribe to mouth
-    FaceTracking.face(0).mouth.openness.monitor({ fireOnInitialValue: true }).subscribe(event => {
-      let openness = event.newValue
-      if (openness > 0.3 && isPlaying) { // select word
-        Time.clearInterval(intervalTimer)
-        const chosenWord = choices[i]
-        Diagnostics.log('you picked: ')
-        Diagnostics.log(chosenWord)
-        storyText.text = storyText.text + chosenWord
-      }
-    })
-    
-    Diagnostics.log("we're playing the game!")
-  } else { // we just shut off the game
-    storyText.text = 'Tap to play game'
-    optionText.text = ''
-    Diagnostics.log('helloooo')
+      Diagnostics.log("we're playing the game!");
+    } else {
+      // we just shut off the game
+      storyText.text = 'Tap to play game';
+      optionText.text = '';
+      Diagnostics.log('helloooo');
+    }
   }
-});
+);
