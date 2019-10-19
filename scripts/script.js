172718e4f5d73435fac5c6fc68d138dd4b0b3f1f
David Adewoyin <dadewoyin16@gmail.com>
2019-10-19 12:24:12 -0400

    start and end game

--- scripts/script.js
+++ scripts/script.js
@@ -7,61 +7,88 @@
 // Changelogs - https://fb.me/spark-changelog
 //==============================================================================
 
-// How to load in modules
-const Diagnostics = require('Diagnostics');
-const Scene = require('Scene');
 
-const storyText = Scene.root.find('2dTextStory');
-storyText.text = 'This is the story!';
-
-const optionText = Scene.root.find('2dTextOption');
-optionText.text = '';
-
-class Queue {
-  constructor() {
-    this._storage = [];
-  }
-
-  enqueue(val) {
-    this._storage.push(val);
-  }
-
-  dequeue() {
-    return this._storage.shift();
-  }
-
-  size() {
-    return this._storage.length;
+const story = {
+  'beach': {
+    after: ['and', 'they', 'forgot', 'their'],
+    children: {
+      'cat': {
+        '.': '.'
+      },
+      'cheetos': {
+        '.': '.'
+      },
+      'towels': {
+        '.': '.'
+      }
+    }
+  },
+  'mall': {
+    after: ['and', 'they', 'bought', 'new'],
+    children: {
+      'slippers': {
+        '.': '.'
+      },
+      'toothpaste': {
+        '.': '.'
+      },
+      'Facebook Portals': {
+        '.': '.'
+      }
+    }
+  },
+  'castle': {
+    after: ['and', 'fell', 'into', 'the'],
+    children: {
+      'well': {
+        '.': '.'
+      },
+      'toilet': {
+        '.': '.'
+      },
+      'moat': {
+        '.': '.'
+      }
+    }
   }
 }
 
-const showOptions = node => {
-  const choicesQueue = new Queue();
 
-  for (let i = 0; i < node.children.length; i++) {
-    choicesQueue.enqueue(node.children[i]);
-  }
 
-  while (choicesQueue.size()) {
-    const choiceNode = choicesQueue.dequeue();
-    choicesQueue.enqueue(choiceNode);
-
-    optionText.text = choiceNode.word;
-
-    // setTimeout()
-    // TODO: add logic to select a node
-  }
-};
-
-const selectNode = () => {};
-// How to log messages to the console (uncomment line below to activate)
-Diagnostics.log(optionText.text.pinLastValue());
-
-// Face Tracking
-
+// How to load in modules
+const Diagnostics = require('Diagnostics');
+const Scene = require('Scene');
 const FaceTracking = require('FaceTracking');
+const TouchGestures = require('TouchGestures');
 
 const face = FaceTracking.face(0);
+const storyText = Scene.root.find('2dTextStory');
+storyText.text = 'Tap to play game';
 
-// Diagnostics.log(face.mouth);
-Diagnostics.log(face.mouth.openness.pinLastValue());
+// const optionText = Scene.root.find('2dTextOption');
+// optionText.text = '';
+
+// FaceTracking.face(0).mouth.openness.monitor({ fireOnInitialValue: true }).subscribe(event => {
+//   Diagnostics.log(event)
+// })
+
+let isPlaying = false
+
+TouchGestures.onTap().subscribeWithSnapshot({
+  // Get the value of mouth openness when the tap gesture is detected
+  'mouthOpennessValue': FaceTracking.face(0).mouth.openness,
+}, function (gesture, snapshot) {
+  Diagnostics.log('hello')
+  // Diagnostics.log(snapshot.isPlaying)
+
+  isPlaying = !isPlaying
+  Diagnostics.log(isPlaying)
+
+  if (isPlaying) {
+    // subscribe to mouth
+    Diagnostics.log("we're playing the game!")
+    storyText.text = "Play"
+  } else { // we just shut off the game
+    storyText.text = 'Tap to play game'
+  }
+});
